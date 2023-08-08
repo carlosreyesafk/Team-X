50efc5522593ef6d09ab8f2c22750e103e33ca2d
Carlos Reyes <carlosreyesafk@gmail.com>
2023-08-08 18:14:09 -0400

    Nueva funcion

--- Programas en C/Prueba 1 C..cpp
+++ Programas en C/Prueba 1 C..cpp
@@ -2,26 +2,40 @@
 
 void nombre(){
 	char nombre[3];
+	int edad;
 	
-	printf("Cual es tu nombre: ");
-	scanf("%s", &nombre);
+	printf("Cual es tu nombre: \n");
+	scanf("%s", &nombre[0]);
 	
-	printf("\n Hola Como estas %s ?", nombre);
+	printf("\n Hola Como estas %s? \n", nombre);
+	scanf("%s", &nombre[0]);
+	
+	
+	printf("\n %s asi te sientes que raro\nQue edad tienes? \n", nombre);
+	scanf("%i", &edad);
+	
+	if(edad >= 18){
+		printf("\nEres mayor que bien");
+	}
+	else{
+		printf("\nSon 30 en najayo");
+	}
+	
+	printf("\nHagamos un recuento:");
+	
+	printf("\nTu nombre es: %s", nombre);
+	printf("\nTe sientes: %s:", nombre);
+	printf("\nY tu edad es: %i :", edad);
+	printf("\nSolo te falta ser virgen");
+	scanf("%i", &edad);
+	
+
 	
 }
 
 int main(){
 	
-	int x;
-	char y;
-	
-	
-	printf ("introduce tu nombre: ");
-	scanf ("%c", &y);
-	printf("\nHola .");
-	
-	scanf("%i", &x);
-	
+	nombre();
 
 	return 0;
 }
