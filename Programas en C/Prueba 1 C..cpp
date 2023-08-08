bf1a86f0a61b18800e7187a40f49b423e4f9be55
Carlos Reyes <carlosreyesafk@gmail.com>
2023-08-08 17:54:20 -0400

    Agregue funcion

--- Programas en C/Prueba 1 C..cpp
+++ Programas en C/Prueba 1 C..cpp
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+void nombre(){
+	char nombre[3];
+	
+	printf("Cual es tu nombre: ");
+	scanf("%s", &nombre);
+	
+	printf("\n Hola Como estas %s ?", nombre);
+	
+}
+
 int main(){
 	
 	int x;
 	char y;
+	
 	
 	printf ("introduce tu nombre: ");
 	scanf ("%c", &y);
